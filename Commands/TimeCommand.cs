cc11ed676654445376315257ccc1af1a78223d78
Greenorine <braian.kant@yandex.ru>
2020-05-19 19:12:02 +0500

    Upload

---
+++ Commands/TimeCommand.cs
@@ -0,0 +1,40 @@
+﻿using Rocket.API;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UP = Rocket.Unturned.Player.UnturnedPlayer;
+
+namespace MyFirstPlugin.Commands
+{
+    class TimeCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+
+        public string Name => "time";
+
+        public string Help => "Выводит Московское время в чат";
+
+        public string Syntax => "/time";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>();
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            var up = (UP)caller;
+            if (command.Length != 0)
+            {
+                ChatManager.serverSendMessage($"[<color=green>Time</color>] Используйте: {Syntax} - {Help}", Color.white, null, up.SteamPlayer(), EChatMode.SAY, null, true);
+                return;
+            }
+            var timeZone = TimeZoneInfo.FindSystemTimeZoneById("Russian Standart Time");
+            var time = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timeZone);
+            ChatManager.serverSendMessage($"[<color=green>Time</color>] МСК Время: {time}", Color.white, null, up.SteamPlayer(), EChatMode.SAY, null, true);
+        }
+    }
+}
